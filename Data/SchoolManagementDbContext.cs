222df1ea024b888111f051838f1441db16a14447
Earl Clark <you@example.com>
2025-06-27 22:59:11 -0400

    Data

---
+++ Data/SchoolManagementDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagementApp.MVC.Data;
+
+public partial class SchoolManagementDbContext : DbContext
+{
+    public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC0790C4C80B");
+
+            entity.Property(e => e.Code).HasMaxLength(5);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Lecture>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC07EB2F8307");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC0796125922");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
